d18f6823eab65c60a6127b33671fc991f6ee1386
JonathanEveillard <jonathaneveillard@cmail.carleton.ca>
2024-08-22 17:05:15 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB6qX_gQXyHtkRwWb49vyNT1yB47Zg68RQ",
-  authDomain: "flashcard-e287f.firebaseapp.com",
-  projectId: "flashcard-e287f",
-  storageBucket: "flashcard-e287f.appspot.com",
-  messagingSenderId: "221531639474",
-  appId: "1:221531639474:web:79b845ab6753b3cd3083ab",
-  measurementId: "G-BX8WWMRMVM"
+  apiKey: "EnterKeyHere",
+  authDomain: "EnterInformation Here",
+  projectId: "EnterInformation Here",
+  storageBucket: "EnterInformation Here",
+  messagingSenderId: "EnterInformation Here",
+  appId: "EnterInformation Here",
+  measurementId: "EnterInformation Here"
 };
 
 // Initialize Firebase
